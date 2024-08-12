d16addd9bd0df1ea6efcaef49e66c6d39af06a32
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-08-12 15:24:30 -0400

    Created Players Jump Configs

--- Game-2D-Platformer/Assets/Scripts/Player/Player.cs
+++ Game-2D-Platformer/Assets/Scripts/Player/Player.cs
@@ -7,7 +7,14 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private float xInput;
 
@@ -20,8 +27,15 @@ public class Player : MonoBehaviour
     private void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
-        HandleAnimation();
 
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        HandleAnimation();
         HandleMovement();
     }
 
@@ -33,5 +47,11 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position,
+            new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
