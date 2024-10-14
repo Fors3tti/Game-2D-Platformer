723b68a30461c8dc341f39fe9df1271aa595df26
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2024-10-13 23:20:19 -0400

    Added First Collectable Item

--- Game-2D-Platformer/Assets/Scripts/DamageTrigger.cs
+++ Game-2D-Platformer/Assets/Scripts/DamageTrigger.cs
@@ -6,7 +6,9 @@ public class DamageTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
-            GameManager.instance.player.Knockback();
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+            player.Knockback();
     }
 }
