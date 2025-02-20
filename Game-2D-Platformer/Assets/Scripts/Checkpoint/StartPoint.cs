9af12f2085bc39b56b7848247cd2e8f08cc2ff4b
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-02-20 13:16:29 -0400

    Created StartPoint Script

--- Game-2D-Platformer/Assets/Scripts/Checkpoint/StartPoint.cs
+++ Game-2D-Platformer/Assets/Scripts/Checkpoint/StartPoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class StartPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Animator anim => GetComponent<Animator>();
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+            anim.SetTrigger("activate");
     }
 }
