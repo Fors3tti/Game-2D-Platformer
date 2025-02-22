295413e0188eddd952f69011337f79c7ff61dd68
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-02-22 13:20:04 -0400

    Added Waypoint Configs in Trap Saw

--- Game-2D-Platformer/Assets/Scripts/Traps/Saw/Trap_Saw.cs
+++ Game-2D-Platformer/Assets/Scripts/Traps/Saw/Trap_Saw.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Trap_Saw : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform[] waypoint;
+
+    public int wayPointIndex = 1;
+
+    private void Start()
     {
-        
+        transform.position = waypoint[0].position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(
+            transform.position, waypoint[wayPointIndex].position, moveSpeed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, waypoint[wayPointIndex].position) < .1f)
+        {
+            wayPointIndex++;
+
+            if (wayPointIndex >= waypoint.Length)
+                wayPointIndex = 0;
+        }
     }
 }
