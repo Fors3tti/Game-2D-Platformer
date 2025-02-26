99bb915a3cd46e4a2dee4be41e9788cc07570a07
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-02-26 12:35:00 -0400

    Added FallingPlatform Movement

--- Game-2D-Platformer/Assets/Scripts/Traps/FallingPlatform/Trap_FallingPlatform.cs
+++ Game-2D-Platformer/Assets/Scripts/Traps/FallingPlatform/Trap_FallingPlatform.cs
@@ -6,7 +6,9 @@ public class Trap_FallingPlatform : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float travelDistance;
-    public Vector3[] wayPoints;
+    private Vector3[] wayPoints;
+    private int wayPointIndex;
+    public bool canMove;
 
     private void Start()
     {
@@ -21,5 +23,27 @@ public class Trap_FallingPlatform : MonoBehaviour
 
         wayPoints[0] = transform.position + new Vector3(0, yOffset, 0);
         wayPoints[1] = transform.position + new Vector3(0, -yOffset, 0);
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (canMove == false)
+            return;
+
+        transform.position = Vector2.MoveTowards(
+            transform.position, wayPoints[wayPointIndex], speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, wayPoints[wayPointIndex]) < .1f)
+        {
+            wayPointIndex++;
+
+            if (wayPointIndex >= wayPoints.Length)
+                wayPointIndex = 0;
+        }
     }
 }
