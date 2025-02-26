58842ea69a2ee6218231e3ac0544b693c03e3b9a
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-02-26 02:27:03 -0400

    Updated Trap Arrow Configs

--- Game-2D-Platformer/Assets/Scripts/Traps/Arrow/Trap_Arrow.cs
+++ Game-2D-Platformer/Assets/Scripts/Traps/Arrow/Trap_Arrow.cs
@@ -8,10 +8,29 @@ public class Trap_Arrow : Trap_Trampoline
     [SerializeField] private float cooldown;
     [SerializeField] private bool rotationRight;
     [SerializeField] private float rotationSpeed;
-
     private int direction = -1;
+    [Space]
+    [SerializeField] private float scaleUpSpeed;
+    [SerializeField] private Vector3 targetScale;
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(.3f, .3f, .3f);
+    }
 
     private void Update()
+    {
+        HandleScaleUp();
+        HandleRotation();
+    }
+
+    private void HandleScaleUp()
+    {
+        if (transform.localScale.x < targetScale.x)
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, scaleUpSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
     {
         direction = rotationRight ? -1 : 1;
 
