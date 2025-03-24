ff49c1c9ec748c9bdf88755f3d9a5e0ba668f0e7
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-03-23 23:37:08 -0400

    Added Enemies Rino Detected Player

--- Game-2D-Platformer/Assets/Scripts/Enemies/Rino/Enemy_Rino.cs
+++ Game-2D-Platformer/Assets/Scripts/Enemies/Rino/Enemy_Rino.cs
@@ -8,12 +8,33 @@ public class Enemy_Rino : Enemy
     [SerializeField] private float detectionRange;
     private bool playerDetected;
 
+    protected override void Update()
+    {
+        base.Update();
+
+        anim.SetFloat("xVelocity", rb.velocity.x);
+
+        HandleCollision();
+        HandleCharge();
+    }
+
+    private void HandleCharge()
+    {
+        if (canMove == false)
+            return;
+
+        rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    }
+
     protected override void HandleCollision()
     {
         base.HandleCollision();
 
         playerDetected = Physics2D.Raycast(transform.position,
             Vector2.right * facingDir, detectionRange, whatIsPlayer);
+
+        if (playerDetected)
+            canMove = true;
     }
 
     protected override void OnDrawGizmos()
