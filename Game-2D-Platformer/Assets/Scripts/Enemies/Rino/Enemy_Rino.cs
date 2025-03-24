aefc3b244a3229b9ce99dc04928d9ef847525ec7
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-03-23 20:03:11 -0400

    Updated Enemies Rino Script

--- Game-2D-Platformer/Assets/Scripts/Enemies/Rino/Enemy_Rino.cs
+++ Game-2D-Platformer/Assets/Scripts/Enemies/Rino/Enemy_Rino.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Rino : MonoBehaviour
+public class Enemy_Rino : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Rino Details")]
+    [SerializeField] private float detectionRange;
+    private bool playerDetected;
+
+    protected override void HandleCollision()
     {
-        
+        base.HandleCollision();
+
+        playerDetected = Physics2D.Raycast(transform.position,
+            Vector2.right * facingDir, detectionRange, whatIsPlayer);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnDrawGizmos()
     {
-        
+        base.OnDrawGizmos();
+
+        Gizmos.DrawLine(transform.position,
+            new Vector2(transform.position.x + (detectionRange * facingDir), transform.position.y));
     }
 }
