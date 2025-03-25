04a663689f9718af055a189153616b52f9e00c69
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-03-25 12:23:26 -0400

    Added Enemies Plant Attack Configs

---
+++ Game-2D-Platformer/Assets/Scripts/Enemies/Plant/Enemy_Plant.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Plant : Enemy
+{
+    [Header("Plant Details")]
+    [SerializeField] private float attackCooldown;
+    public float lastTimeAttacked;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        bool canAttack = Time.time > lastTimeAttacked + attackCooldown;
+
+        if (isPlayerDetected && canAttack)
+            Attack();
+    }
+
+    private void Attack()
+    {
+        lastTimeAttacked = Time.time;
+        anim.SetTrigger("attack");
+    }
+
+    protected override void HandleAnimator()
+    {
+        //Keep it empty, unless you need to update parameters
+    }
+}
