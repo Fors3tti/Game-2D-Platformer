3be842477ce97029ab003d610e6e55ced3afca39
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-03-31 15:29:54 -0400

    Updated Credit Scenes Configs

--- Game-2D-Platformer/Assets/Scripts/UI_Credits.cs
+++ Game-2D-Platformer/Assets/Scripts/UI_Credits.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_Credits : MonoBehaviour
 {
     [SerializeField] private RectTransform rectTrans;
     [SerializeField] private float scrollSpeed = 200;
+    [SerializeField] private float offScreenPosition = 1500;
+
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+    private bool creditsSkipped;
 
     private void Update()
     {
         rectTrans.anchoredPosition += Vector2.up * scrollSpeed * Time.deltaTime;
+
+        if (rectTrans.anchoredPosition.y > offScreenPosition)
+            GoToMainMenu();
+    }
+
+    public void SkipCredits()
+    {
+        if(creditsSkipped == false)
+        {
+            scrollSpeed *= 10;
+            creditsSkipped = true;
+        }
+        else
+        {
+            GoToMainMenu();
+        }
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 }
