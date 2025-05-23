c337b0a503fff18cc2aa8ac6789a1fd00a44c9fc
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-05-23 19:18:24 -0400

    Updated Levels Button Infos

--- Game-2D-Platformer/Assets/Scripts/UI/UI_LevelButton.cs
+++ Game-2D-Platformer/Assets/Scripts/UI/UI_LevelButton.cs
@@ -5,8 +5,12 @@ using TMPro;
 public class UI_LevelButton : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI levelNumberText;
+
+    [SerializeField] private TextMeshProUGUI bestTimeText;
+    [SerializeField] private TextMeshProUGUI fruitsText;
+
     private int levelIndex;
-    public string sceneName;
+    private string sceneName;
 
     public void SetupButton(int newLevelIndex)
     {
@@ -14,10 +18,30 @@ public class UI_LevelButton : MonoBehaviour
 
         levelNumberText.text = "Level " + levelIndex;
         sceneName = "Level_" + levelIndex;
+
+        bestTimeText.text = TimerInfoText();
+        fruitsText.text = FruitsInfoText();
     }
 
     public void LoadLevel()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private string FruitsInfoText()
+    {
+        int totalFruits = PlayerPrefs.GetInt("Level" + levelIndex + "TotalFruits", 0);
+        string totalFruitsText = totalFruits == 0 ? "?" : totalFruits.ToString();
+
+        int fruitsCollected = PlayerPrefs.GetInt("Level" + levelIndex + "FruitsCollected");
+
+        return "Fruits: " + fruitsCollected + " / " + totalFruits;
+    }
+
+    private string TimerInfoText()
+    {
+        float timerValue = PlayerPrefs.GetFloat("Level" + levelIndex + "BestTime", 99);
+
+        return "Best Time: " + timerValue.ToString("00");
     }
 }
