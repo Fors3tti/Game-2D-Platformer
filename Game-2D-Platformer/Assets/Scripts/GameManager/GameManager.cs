c101a209918b09fbabaa8311c328bd703be46dce
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2025-05-29 12:41:54 -0400

    Created Main Menu Transitions Configs

--- Game-2D-Platformer/Assets/Scripts/GameManager/GameManager.cs
+++ Game-2D-Platformer/Assets/Scripts/GameManager/GameManager.cs
@@ -149,7 +149,11 @@ public class GameManager : MonoBehaviour
         PlayerPrefs.SetInt("Level" + nextLevelIndex + "Unlocked", 1);
 
         if (NoMoreLevels() == false)
+        {
             PlayerPrefs.SetInt("ContinueLevelNumber", nextLevelIndex);
+            PlayerPrefs.SetInt("LastUsedSkin", SkinManager.instance.GetSkinId());
+        }
+            
     }
 
     public void RestartLevel()
